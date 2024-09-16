74b2ab2232c94e61e460fb25018de1b78758f677
Faisal Albasu <albasfaisal@yandex.ru>
2024-09-16 17:52:49 +0500

    removed old files

--- erg dashboard/gui/gui_app/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class SigFile(models.Model):
-    description = models.CharField(max_length=1000, blank = True)
-    file = models.FileField(upload_to='files/')
-    uploaded_at = models.DateTimeField(auto_now_add=True)
