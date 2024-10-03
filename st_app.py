7e7e42e99cfea4858c694e19f32d8044bae53a55
Faisal Albasu <albasfaisal@yandex.ru>
2024-10-03 18:44:14 +0500

    demo version 0.1

--- st_app.py
+++ st_app.py
@@ -1,43 +1,58 @@
-from pyarrow import duration
 import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import pickle
+import scipy
 
-# def display_navbar():
-#     col1, col2 = st.columns([2, 1])
+def load_signal(sig):
+    assert type(sig) is str, f"{type(sig)}"
+    signal = np.array(df[sig])
+    return signal
     
-#     with col1:
-#         st.button("Home", key = "home")
+def plot_signal(sig_no):
+    fig, ax = plt.subplots()
+    # sig = np.array(df[sig_no])
+    sig = load_signal(sig_no)
+    duration = np.array(df.index)
+    ax.plot(duration, sig)
+    st.pyplot(fig)
     
-#     with col2:
-#         st.button("Upload", key = "upload")
-        
-# def upload_file():
-#     uploaded_file = st.file_uploader("Choose a CSV file to upload", type = "csv")
+def plot_spectrogram(sig_no):
+    fig, ax = plt.subplots()
+    # sig = np.array(df[sig_no])
+    sig = load_signal(sig_no)
+    sxx, freqs, bins, im = ax.specgram(sig, Fs = 1 / 0.005, NFFT = 128, noverlap = 32, cmap = 'jet')
+    st.pyplot(fig)
     
-#     if uploaded_file is not None:
-#         df = pd.read_csv(uploaded_file, index_col='Time,ms')
-        
-#         options = df.columns.tolist()
+def compute_features(sig_no):
+    # sig = np.array(df[sig_no])
+    sig = load_signal(sig_no)
+    sxx, freqs, bins= scipy.signal.spectrogram(sig, fs = 1 / 0.005, nperseg = 128, noverlap = 32)
+    spectrum = sxx.flatten()
+    bmin = np.min(spectrum)
+    bmedian = np.median(spectrum)
+    bmax = np.max(spectrum)
+    bmean = np.mean(spectrum)
+    features_dict = {
+        'Bmin': bmin,
+        'Bmax': bmax,
+        'Bmedian': bmedian,
+        'Bmean': bmean,
+        # 'ids': sig
+    }
 
-#     return df, options
+    features_df = pd.DataFrame(features_dict, index = range(0, len(features_dict)))
+    features_df.drop_duplicates(inplace=True)
+
+    return features_df
     
-# def plot_signal():
-#     df, options = upload_file()
+with open('model.pkl', 'rb') as model:
+    loaded_model = pickle.load(model)
     
-#     selected_column = st.selectbox("Select Signal", options)
-    
-#     if selected_column:
-#         st.line_chart(df[selected_column])
-        
-        
-# def main():
-#     display_navbar()
-    
-#     if st.session_state.get("upload"):
-#         upload_file()
-# 
+with open('encoder.pkl', 'rb') as encoder:
+    loaded_encoder = pickle.load(encoder)
+
 col1, col2 = st.columns([3, 1])
 
 with col2:
@@ -50,18 +65,78 @@ if uploaded_file is not None:
     
     options = df.columns.tolist()
     
-    selected_column = st.selectbox("Select Signal", options)
+    selected_column = st.selectbox("Select Signal", options, key = "signal_select")
     
     if selected_column:
         sigplot, spectrogram = st.columns([1, 1])
+        
         with sigplot:
-            fig, ax = plt.subplots()
-            sig = np.array(df[selected_column])
-            duration = np.array(df.index)
-            ax.plot(duration, sig)
-            st.pyplot(fig)
+            plot_signal(selected_column)
         with spectrogram:
-            fig, ax = plt.subplots()
-            sig = np.array(df[selected_column])
-            ax.specgram(sig, Fs = 1 / 0.005, NFFT = 128, noverlap = 32, cmap = 'jet')
-            st.pyplot(fig)
+            plot_spectrogram(selected_column)
+        
+        features = compute_features(selected_column)
+        # st.write(features)
+        
+        if features is not None:    
+            prediction = loaded_model.predict(features)
+            prediction_probs = loaded_model.predict_proba(features)
+            # st.write(prediction, prediction_probs)
+            
+            probs_plot, fi_plot = st.columns([1, 1])
+                    
+            with probs_plot:
+                fig, ax = plt.subplots()
+                ax.barh(loaded_encoder.classes_, prediction_probs[0])
+                st.pyplot(fig)
+            
+            with fi_plot:
+                fig, ax = plt.subplots()
+                ax.barh(loaded_model.model.estimator.feature_name_, loaded_model.model.estimator.feature_importances_)
+                st.pyplot(fig)
+        
+    # with sigplot:
+    #     fig, ax = plt.subplots()
+    #     sig = np.array(df[selected_column])
+    #     duration = np.array(df.index)
+    #     ax.plot(duration, sig)
+    #     st.pyplot(fig)
+    # with spectrogram:
+    #     fig, ax = plt.subplots()
+    #     sig = np.array(df[selected_column])
+    #     sxx, freqs, bins, im = ax.specgram(sig, Fs = 1 / 0.005, NFFT = 128, noverlap = 32, cmap = 'jet')
+    #     st.pyplot(fig)
+    
+    
+    
+    # if selected_column:
+    #     sigplot, spectrogram = st.columns([1, 1])
+    #     with sigplot:
+    #         fig, ax = plt.subplots()
+    #         sig = np.array(df[selected_column])
+    #         duration = np.array(df.index)
+    #         ax.plot(duration, sig)
+    #         st.pyplot(fig)
+    #     with spectrogram:
+    #         fig, ax = plt.subplots()
+    #         sig = np.array(df[selected_column])
+    #         ax.specgram(sig, Fs = 1 / 0.005, NFFT = 128, noverlap = 32, cmap = 'jet')
+    #         st.pyplot(fig)
+            
+    #     with open('model.pkl', 'rb') as model:
+    #         loaded_model = pickle.load(model)
+            
+    #         prediction = loaded_model.predict(df[selected_column])
+            
+    #         prediction_probs = loaded_model.predict_proba(df[selected_column])
+    #         target_labels = np.array('healthy', 'unhealthy')
+            
+    #         probs_plot, fi_plot = st.columns([1, 1])
+            
+    #         with probs_plot:
+    #             fig, ax = plt.subplots()
+    #             ax.barh(target_labels, prediction_probs)
+            
+    #         with fi_plot:
+    #             fig, ax = plt.subplots()
+    #             ax.barh(loaded_model.model.estimator.feature_name_, loaded_model.model.estimator.feature_importances_)
--- train.py
+++ train.py
@@ -1,15 +1,17 @@
-# %%
 import numpy as np
 import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import seaborn as sns
-from flaml import AutoML
+from flaml.automl.automl import AutoML
 import pickle
 import scipy
+from sklearn import preprocessing as prep
+from sklearn.model_selection import train_test_split
 
-# %%
+
 max_balanced = pd.read_csv('max_balanced.csv', index_col='Time,ms')
+max_bal_diag = pd.read_csv('max_balanced_diagnoses.csv', index_col = 0)
 def load_signal(sig):
     assert type(sig) is str, f"{type(sig)}"
     signal_no = np.array(max_balanced[sig])
@@ -31,6 +33,49 @@ def compute_features(sig, nperseg, noverlap, window):
     bmean = np.mean(spectrum)
     return bmin, bmedian, bmax, bmean
     
-# %%
+
 dfeatures = pd.DataFrame(columns = ['params', 'Bmin', 'Bmedian', 'Bmax', 'Bmean', 'pat_no'])
-print(dfeatures)
+
+window, size, overlap = 'boxcar', 128, 64
+for col in max_balanced.columns:
+
+    signal_no = load_signal(col)
+#     freqs, times, Sxx = compute_spectrogram(signal_no, nperseg = window_size, noverlap = overlap, window = window_type)
+    bmin, bmedian, bmax, bmean = compute_features(signal_no, size, overlap, window)
+
+    # Create a dictionary with the features
+    features_dict = {
+        'params': f"{window}{size}{overlap}",
+        'Bmin': bmin,
+        'Bmedian': bmedian,
+        'Bmax': bmax,
+        'Bmean': bmean,
+        'pat_no': col
+    }
+
+    features = pd.DataFrame(features_dict, index = range(0, len(features_dict)))
+
+    dfeatures = pd.concat([dfeatures, features], ignore_index = True)
+    
+dfeatures.duplicated()
+dfeatures.drop_duplicates(inplace = True, ignore_index = True)
+dfeatures.drop(['params', 'pat_no'], axis = 1, inplace = True)
+
+features, targets = dfeatures, max_bal_diag['Diagnosis'].to_numpy()
+encoder = prep.LabelEncoder()
+encoder.fit(targets)
+target_labels, encoded_targets = encoder.classes_, encoder.transform(targets)
+
+random_state = 42
+X_train, X_test, y_train, y_test = train_test_split(features, encoded_targets, test_size = 0.1, random_state = random_state)
+
+automl = AutoML()
+
+automl.fit(X_train, y_train,
+          task = 'classification',
+          metric = 'accuracy',
+          time_budget = 10800,
+          estimator_list = ['lgbm', 'xgboost', 'rf', 'histgb'],
+          eval_method = 'cv',
+          split_type = 'stratified',
+          n_splits = 5)
